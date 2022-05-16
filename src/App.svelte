e9316adc67a3b33a88ca44669336510082898128
anotherempty <anotherempty@outlook.com>
2022-05-16 08:33:28 +0300

    implemented infinite loading

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,118 @@
 <script>
-	export let name;
+  import Gallery from "svelte-brick-gallery";
+
+  /**
+   * @type {string | any[]}
+   */
+  let images;
+
+  let height = 120;
+  let pad = 10;
+
+  const scale = (w, h) => {
+    return Math.floor((height * w) / h);
+  };
+
+  const load = (async () => {
+    let res = await fetch(`https://picsum.photos/v2/list?page=1&limit=30`);
+
+    images = (await res.json()).map((img) => ({
+      src: `https://picsum.photos/id/${img.id}/${
+        scale(img.width, img.height) + pad
+      }/${height + pad}`,
+      id: img.id,
+      width: img.width,
+      height: img.height,
+    }));
+
+    return images;
+  })();
+
+  let page = 4;
+  const more = async () => {
+    let res = await fetch(`https://picsum.photos/v2/list?page=${page}&limit=10`);
+
+    images = images.concat(
+      (await res.json()).map((img) => ({
+        src: `https://picsum.photos/id/${img.id}/${
+          scale(img.width, img.height) + pad
+        }/${height + pad}`,
+        width: img.width,
+        height: img.height,
+        author: img.author,
+      }))
+    );
+    page++;
+
+    return images;
+  };
+
+  let scroll = 0;
+  let winHeight = 0;
+  let bodyHeight = 0;
+  let o = 0;
+
+  $: if (scroll > 0 && winHeight > 0) {
+    if (Math.ceil(scroll + winHeight) >= bodyHeight) {
+      more();
+    }
+  };
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<svelte:window
+  bind:scrollY={scroll}
+  bind:innerHeight={winHeight}
+/>
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+<div class="flex" bind:clientHeight={bodyHeight}>
+  <div class="w-1/3">
+    <div class="w-1/3 h-screen fixed">
+      {scroll}
+      <button on:click={() => more()}> load </button>
+      {winHeight} <br />
+      {bodyHeight}
+    </div>
+  </div>
+  <div class="flex-1 overflow-visible">
+    {#await load}
+      loading...
+    {:then data}
+      <div style="">
+        <Gallery
+          {images}
+          justify="left"
+          itemHeight={150}
+          itemWidth={500}
+          gap={16}
+        >
+          <div
+            slot="image"
+            let:index
+            let:style
+            style="height:100%; position:relative;"
+          >
+            <img
+              src={images[index].src}
+              style={style + "  border-radius: 12px;"}
+              alt={index}
+            />
+          </div>
+        </Gallery>
+      </div>
+    {:catch}
+      loading error
+    {/await}
+  </div>
+</div>
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+<style lang="postcss" global>
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  html,
+  body {
+    margin: 0;
+    padding: 0;
+  }
 </style>
