d66a0bafa5e03f784e7fc3f26b81343c88e34d2a
anotherempty <anotherempty@outlook.com>
2022-05-22 17:33:04 +0300

    added props store

---
+++ src/lib/props.js
@@ -0,0 +1,6 @@
+import { writable } from "svelte/store";
+
+export const rounded = writable(false);
+export const animated = writable(false);
+export const hoverEffect = writable(false);
+export const infiniteLoad = writable(true);
